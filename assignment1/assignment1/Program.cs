ae7cf967a7da5fd673509363571640a2fdc804cc
likithatv2000 <likitha.tv@gmail.com>
2022-08-09 18:03:25 +0530

    Adding assignment1

--- assignment1/assignment1/Program.cs
+++ assignment1/assignment1/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace assignment1
+﻿/*namespace assignment1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char c, c1, c2;
+
+            Console.Write("Input first letter: ");
+            c = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input second letter: ");
+            c1= Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input three letter: ");
+            c2 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2}", c2, c1, c);
+        }
+    }
+}
+
+*/
+/*  namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Radius of circle: ");
+            double R= Convert.ToDouble(Console.ReadLine());
+            double Area = Math.PI * R* R;
+            Console.WriteLine("Area of circle: " + Area);
+            double perimeter = 2 * Math.PI * R;
+            Console.WriteLine("Perimeter of circle: " + perimeter);
+        }
+    }
+}
+*/
+
+namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Radius of circle: ");
+            double R= Convert.ToDouble(Console.ReadLine());
+            double Area = Math.PI * R* R;
+            Console.WriteLine("Area of circle: " + Area);
+            double perimeter = 2 * Math.PI * R;
+            Console.WriteLine("Perimeter of circle: " + perimeter);
         }
     }
 }
