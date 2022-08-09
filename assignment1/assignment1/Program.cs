108b2f9abed01805b9266562c535fb194b60b2f8
likithatv2000 <likitha.tv@gmail.com>
2022-08-09 19:38:17 +0530

    Adding assi

--- assignment1/assignment1/Program.cs
+++ assignment1/assignment1/Program.cs
@@ -1,4 +1,5 @@
-﻿/*namespace assignment1
+﻿//Write a C# Sharp program that takes three letters as input and display them in reverse order
+namespace assignment1
 {
     internal class Program
     {
@@ -20,8 +21,11 @@
     }
 }
 
-*/
-/*  namespace assignment1
+
+//Write a C# Sharp program that takes the radius of a circle as input and calculate the perimeter and area of the circle.
+
+
+  namespace assignment1
 {
     internal class Program
     {
@@ -37,7 +41,102 @@
         }
     }
 }
-*/
+
+// Write a C# Sharp program that takes distance and time as input and displays the speed in kilometres per hour and miles per hour.
+/namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float distance;
+            float hour, min, sec;
+
+            float timeSec;
+            float mps;
+            float kph, mph;
+
+            Console.Write("Input distance(metres): ");
+            distance = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Input timeSec(hour): ");
+            hour = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Input timeSec(minutes): ");
+            min = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Input timeSec(seconds): ");
+            sec = Convert.ToSingle(Console.ReadLine());
+
+            timeSec = (hour * 3600) + (min * 60) + sec;
+            mps = distance / timeSec;
+            kph = (distance / 1000.0f) / (timeSec / 3600.0f);
+            mph = kph / 1.609f;
+            Console.WriteLine("Your speed in km/h is {0}", kph);
+            Console.WriteLine("Your speed in miles/h is {0}", mph);
+        }
+    }
+}
+
+
+
+//Write a C# Sharp program that takes two numbers as input and returns true or false when both numbers are even or odd.
+
+
+ namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n1, n2;
+            Console.WriteLine("enter first number");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            if (n1 % 2 == 0 && n2 % 2 == 0)
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+
+        }
+    }
+    }
+
+
+
+
+//Write a C# Sharp program to find whether a given year is a leap year or not.
+
+ namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int year;
+            Console.Write("Check whether a given year is leap year or not:\n");
+            Console.Write("Input an year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 400) == 0)
+                Console.WriteLine("{0} is a leap year.\n", year);
+            else if ((year % 100) == 0)
+                Console.WriteLine("{0} is not a leap year.\n",year);
+            else if ((year % 4) == 0)
+                Console.WriteLine("{0} is a leap year.\n", year);
+            else
+                Console.WriteLine("{0} is not a leap year.\n",year);
+
+        }
+    }
+}
+
+
+/*Write a C# Sharp program to accept the height of a person in centimeter and
+categorize the person according to their height. (Below 135 - short, above 180, tall. 
+In between average) */
+
 
 namespace assignment1
 {
@@ -46,12 +145,18 @@ namespace assignment1
         static void Main(string[] args)
         {
 
-            Console.Write("Enter Radius of circle: ");
-            double R= Convert.ToDouble(Console.ReadLine());
-            double Area = Math.PI * R* R;
-            Console.WriteLine("Area of circle: " + Area);
-            double perimeter = 2 * Math.PI * R;
-            Console.WriteLine("Perimeter of circle: " + perimeter);
+
+            float height;
+            Console.WriteLine("Enter  the Height (in centimeters) \n");
+            height = int.Parse(Console.ReadLine());
+            if (height < 150.0)
+                Console.WriteLine("short \n");
+            else if ((height >= 150.0) && (height <= 179.0))
+                Console.WriteLine(" Average Height \n");
+            else if(height >= 180.0)
+                Console.WriteLine("Taller \n");
+           
+
         }
     }
 }
