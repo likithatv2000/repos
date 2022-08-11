6b00e30b8ce88c0d62c56c947c7d10237c50e4d5
likithatv2000 <likitha.tv@gmail.com>
2022-08-11 21:10:40 +0530

    dDay3 assignment

---
+++ OOPs/OOPs/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace OOPs
+{
+    internal class DemoEncap
+    {
+
+        private String studentName;
+        private int studentAge;
+        public String Name
+        {
+
+            get
+            {
+                return studentName;
+            }
+
+            set
+            {
+                studentName = value;
+            }
+
+        }
+
+        public int Age
+        {
+
+            get
+            {
+                return studentAge;
+            }
+
+            set
+            {
+                studentAge = value;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            DemoEncap obj = new DemoEncap();
+            obj.Name = "Likitha";
+            obj.Age = 21;
+            Console.WriteLine("Name: " + obj.Name);
+            Console.WriteLine("Age: " + obj.Age);
+        }
+    }
+}
